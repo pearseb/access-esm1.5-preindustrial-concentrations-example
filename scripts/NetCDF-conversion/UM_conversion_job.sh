330213c30f574499683d2d785aa5238b25e576b9
Spencer Wong <88933912+blimlim@users.noreply.github.com>
2024-08-23 15:18:25 +1000

    Update scripts/NetCDF-conversion/UM_conversion_job.sh
    Co-authored-by: Aidan Heerdegen <aidan.heerdegen@anu.edu.au>

--- scripts/NetCDF-conversion/UM_conversion_job.sh
+++ scripts/NetCDF-conversion/UM_conversion_job.sh
@@ -9,4 +9,7 @@
 module use /g/data/vk83/modules
 module load payu
 
+# Command below calls https://github.com/ACCESS-NRI/um2nc-standalone/blob/main/umpost/conversion_driver_esm1p5.py
+# By default UM atmosphere fields files are deleted after conversion to save space. 
+# Remove --delete-ff command line option to retain original files for testing purposes
 esm1p5_convert_nc $PAYU_CURRENT_OUTPUT_DIR --delete-ff
