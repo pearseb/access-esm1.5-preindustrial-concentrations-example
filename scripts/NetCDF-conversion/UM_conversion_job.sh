475465d2dee4e44ec14758babe85f4bab82ea508
Spencer Wong <spencer.wong@anu.edu.au>
2024-08-23 14:31:16 +1000

    swap to release payu

--- scripts/NetCDF-conversion/UM_conversion_job.sh
+++ scripts/NetCDF-conversion/UM_conversion_job.sh
@@ -6,7 +6,7 @@
 #PBS -l walltime=00:40:00
 #PBS -l wd
 
-module use /g/data/vk83/prerelease/modules
+module use /g/data/vk83/modules
 module load payu
 
 esm1p5_convert_nc $PAYU_CURRENT_OUTPUT_DIR --delete-ff
